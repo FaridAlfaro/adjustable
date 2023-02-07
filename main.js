fc73d8affc7f27a1f68f1f55a922b0978a5c7c74
Farid Alfaro <farix6@gmail.com>
2023-02-07 00:19:02 -0300

    Avance segunda section

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ iconoMenu.addEventListener('click', (e) => {
     const rutaActual = e.target.getAttribute('src');
 
     if(rutaActual == '/img/3869892-200.png'){
-        e.target.setAttribute('src','img/open-menu2.png');
+        e.target.setAttribute('src','/img/open-menu2.png');
     }else{
         e.target.setAttribute('src','/img/3869892-200.png');
     }
